138f4cf420e663aeb584a2708589608ae81e8123
Dustin Collins <dustinrcollins@gmail.com>
2015-09-23 13:33:35 -0400

    Install the Conjur CLI on the host

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CONJUR_CLI_VERSION='4.27.0'
+
 # Install Docker
 apt-key adv \
 --keyserver hkp://p80.pool.sks-keyservers.net:80 \
@@ -13,12 +15,18 @@ apt-get update
 
 apt-get install -y docker-engine
 
+cli_dlpath='/tmp/conjur.deb'
+wget -q \
+"https://s3.amazonaws.com/conjur-releases/omnibus/conjur_${CONJUR_CLI_VERSION}-1_amd64.deb" -O ${cli_dlpath} && \
+dpkg -i ${cli_dlpath} && \
+rm -f ${cli_dlpath}
+
 echo "Loading Conjur appliance image into Docker"
 docker load < '/tmp/conjur-appliance.tar'
 
 container_name='conjur-appliance'
 
-docker rm -f ${container_name}
+docker rm -f ${container_name} || true  # Try to remove the container, even if it doesn't exist
 docker create \
 --name ${container_name} \
 --restart always \
