fd642e771687ca0652af27575156b818546b35f0
Dustin Collins <dustinrcollins@gmail.com>
2015-09-28 16:15:52 -0400

    Fix jenkins script typo

--- jenkins.sh
+++ jenkins.sh
@@ -5,4 +5,4 @@ export PATH=/opt/conjur/bin:$PATH
 APPLIANCE_IMAGE=${1}
 APPLIANCE_IMAGE_TAG=${2}
 
-summon ./build-ami.sh ${APPLIANCE_IMAGE} $){APPLIANCE_IMAGE_TAG}
+summon ./build-ami.sh ${APPLIANCE_IMAGE} ${APPLIANCE_IMAGE_TAG}
