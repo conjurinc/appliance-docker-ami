646570cacb6e85d93c75c10400b6d2a72965406a
Dustin Collins <dustinrcollins@gmail.com>
2016-01-20 17:00:40 -0500

    Use latest version of Ubuntu 14.04, update installed CLI version, add container syslog forwarding

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 
-CONJUR_CLI_VERSION=${CONJUR_CLI_VERSION-'4.27.0'}
+CONJUR_CLI_VERSION=${CONJUR_CLI_VERSION-'4.28.2'}
 APPLIANCE_IMAGE_TAG=${APPLIANCE_IMAGE_TAG-latest}
+
 # Install Docker
 apt-key adv \
 --keyserver hkp://p80.pool.sks-keyservers.net:80 \
@@ -11,8 +12,11 @@ apt-key adv \
 bash -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
 
 apt-get update
+apt-get install -y linux-image-extra-$(uname -r)
 
 apt-get install -y docker-engine
+
+docker run --rm hello-world
 
 # Enable the 'ubuntu' user to manage docker without sudo
 usermod -a -G docker ubuntu
@@ -28,10 +32,11 @@ docker load < '/tmp/conjur-appliance.tar'
 
 container_name='conjur-appliance'
 
-docker rm -f ${container_name} && true  # Try to remove the container, even if it doesn't exist
+docker rm -f ${container_name} || true  # Try to remove the container, even if it doesn't exist
 cid=$(docker create \
 --name ${container_name} \
 --restart always \
+--log-driver=syslog --log-opt tag="${container_name}" \
 -p "443:443" -p "636:636" -p "5432:5432" -p "38053:38053" \
 registry.tld/conjur-appliance:${APPLIANCE_IMAGE_TAG})
 
@@ -45,3 +50,8 @@ script
   /usr/bin/docker start -a ${container_name}
 end script
 CONF
+
+# Installs security patches
+# http://packages.ubuntu.com/trusty-updates/unattended-upgrades
+apt-get install -y ntp unattended-upgrades
+unattended-upgrade -v
