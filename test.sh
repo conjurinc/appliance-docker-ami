7f72b0a5f1a4aeb0cc4fc6319da49c31a610d2a3
Gary Moon <gary@garymoon.net>
2018-09-18 16:00:51 -0400

    Temporarily allow for debugging image

--- test.sh
+++ test.sh
@@ -15,33 +15,15 @@ finish() {
       -e AMI_ID \
       -e AWS_ACCESS_KEY_ID \
       -e AWS_SECRET_ACCESS_KEY \
-      -e COREOS_AMI \
       -v "$SSH_KEY:/root/.ssh/id_rsa" \
       test-kitchen kitchen destroy'
 
   return ${code}
 }
-trap finish EXIT
+#trap finish EXIT
 
 # Create a Test Kitchen container
 docker build -t test-kitchen -f Dockerfile.testkitchen .
-
-echo "Fetching latest CoreOS AMI..."
-export COREOS_AMI=$(summon docker run --rm --env-file @SUMMONENVFILE \
-  mesosphere/aws-cli ec2 describe-images --filters '[
-    {"Name": "owner-id", "Values": ["595879546273"] },
-    {"Name": "name", "Values": ["CoreOS-stable*"] },
-    {"Name": "virtualization-type", "Values": ["hvm"] },
-    {"Name": "architecture", "Values": ["x86_64"] },
-    {"Name": "hypervisor", "Values": ["xen"] },
-    {"Name": "root-device-type", "Values": ["ebs"] },
-    {"Name": "state", "Values": ["available"] }
-    ]' \
-    --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId | [0]' \
-    --region us-east-1 \
-    --output text
-  )
-echo "CoreOS AMI: $COREOS_AMI"
 
 echo "Launching test instance from ${ami_id}"
 
@@ -53,7 +35,6 @@ summon env AMI_ID=${ami_id} bash -c 'docker run \
     -e AMI_ID \
     -e AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY \
-    -e COREOS_AMI \
     -v "$SSH_KEY:/root/.ssh/id_rsa" \
     test-kitchen kitchen converge'
 
