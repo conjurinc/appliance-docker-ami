cacc63eb120e1d03eb9d8fc3eea9ff5d006d9d72
Gary Moon <gary@garymoon.net>
2018-09-19 10:57:47 -0400

    Avoid race condition in test-kitchen test-kitchen runs a little too early on newer versions of CoreOS and expects the container to be running before it actually is. Just sleeping here now, but a poll would be better in future.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,15 @@
 
 # Used by test-kitchen, converge looks for 'bootstrap.sh' by default
 
+echo "Running bootstrap.sh..."
+
+echo "Waiting for container to be ready (TODO switch to polling)"
+sleep 60
+
+echo "Fetching instance hostname..."
 hostname=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 password=testing
 orgaccount=kitchen
 
+echo "Configuring for master..."
 docker exec conjur-appliance evoke configure master -h $hostname -p $password $orgaccount
