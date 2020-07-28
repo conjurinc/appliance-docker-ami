e2f90b837024735d8370b5c16f573ce242fa785c
Gary Moon <gary@garymoon.net>
2020-07-27 20:56:28 -0400

    Address github feedback
    Integrate seccomp
    Add the /opt/cyberark/dap directory convention
    Remove unnecessary published ports
    Convert `docker create` flags to the long variants
    Rename container to 'dap'
    Switch docker restart policy from 'always' to 'unless-stopped'

--- scripts/dap-ami-bootstrap.sh
+++ scripts/dap-ami-bootstrap.sh
@@ -11,7 +11,7 @@ if ! docker images | grep appliance; then
 fi
 
 image_id=$(docker images -q)
-container_name='dap-appliance'
+container_name='dap'
 
 docker rm -f ${container_name} > /dev/null 2>&1 || true
 
@@ -21,17 +21,17 @@ mkdir -p /opt/conjur/backup
 echo "Creating DAP container"
 cid=$(docker create \
 --name ${container_name} \
---privileged --restart always \
+--privileged --restart unless-stopped \
 --log-driver=journald \
--v /var/log/conjur:/var/log/conjur \
--v /opt/conjur/backup:/opt/conjur/backup \
--p "443:443" \
--p "444:444" \
--p "636:636" \
--p "5432:5432" \
--p "5433:5433" \
--p "1999:1999" \
--p "127.0.0.1:38053:38053" \
+--volume /var/log/conjur:/var/log/conjur:Z \
+--volume /opt/conjur/backup:/opt/conjur/backup:Z \
+--volume /opt/cyberark/dap/security:/opt/cyberark/dap/security:Z \
+--security-opt seccomp:/opt/cyberark/dap/security/seccomp.json \
+--publish "443:443" \
+--publish "444:444" \
+--publish "5432:5432" \
+--publish "5433:5433" \
+--publish "1999:1999" \
 $image_id)
 
 cat << CONF > /etc/systemd/system/dap.service
@@ -49,8 +49,8 @@ ExecStop=-/usr/bin/docker stop ${container_name}
 WantedBy=multi-user.target
 CONF
 
-systemctl enable /etc/systemd/system/dap.service
-
 systemctl daemon-reload
+
+systemctl enable /etc/systemd/system/dap.service
 
 echo "DAP container ready (reboot, or use 'systemctl start dap' to start)"
