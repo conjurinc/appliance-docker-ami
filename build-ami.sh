eb66c4c39bb1ebd8e769de2bfb80962f3528172c
Matthew Brace <matthew.brace@cyberark.com>
2023-12-21 10:09:36 -0600

    Update to use full packer build to include amazon-ebs builder

--- build-ami.sh
+++ build-ami.sh
@@ -32,7 +32,7 @@ export PACKER_LOG=1
 summon docker run \
     -v $(pwd):/opt/ \
     --env-file @SUMMONENVFILE -e AMI \
-    hashicorp/packer:light build -var "appliance_image_tag=$TAG" /opt/packer.json | tee packer.out
+    hashicorp/packer:full build -var "appliance_image_tag=$TAG" /opt/packer.json | tee packer.out
 
 # write the AMI ID to files for smoke tests archiving
 ami_id=$(tail -3 packer.out | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')
